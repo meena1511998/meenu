607e10f0fdff1457c65090e515528b5107b04592
meena1511998 <37071102+meena1511998@users.noreply.github.com>
2018-03-13 17:51:36 +0530

    Create sum of N num.java

---
+++ sum of N num.java
@@ -0,0 +1,13 @@
+class Sumnum
+{
+	public static void main(String args[])
+	{
+		int sum = 0;
+		int n=20;
+		for(int i = 1;i <= n;i++)
+		{
+			sum = sum + i;
+		}
+		System.out.println("The Sum Of "+n+" Numbers are:" + sum);
+	}
+}
