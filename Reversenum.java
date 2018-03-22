dcc012de227dda2fcab5dc5560b82ed60fa50d33
meena1511998 <37071102+meena1511998@users.noreply.github.com>
2018-03-22 17:29:35 +0530

    Create Reversenum.java

---
+++ Reversenum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ class Reversenum
+{
+   public static void main(String args[])
+   {
+	Scanner s=new Scanner(System.in);
+	System.out.println("enter number:");
+	int i=n.nextInt();
+	int r=Integer.reverse(i);
+	System.out.println("rev is:"+r);
+	}
+}
