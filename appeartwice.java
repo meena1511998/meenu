e4b4d2fdb67945bbfd1bf5a566972c4ccea649d3
meena1511998 <37071102+meena1511998@users.noreply.github.com>
2018-03-23 17:53:06 +0530

    Create appeartwice.java

---
+++ appeartwice.java
@@ -0,0 +1,29 @@
+import java.io.*;
+class number
+package array;
+ 
+public class FindElementThatAppearOnceRestAllAppearThrice
+{
+ 
+ public static void main(String[] args)
+ {
+  int arr[] = {12, 1, 12, 3, 12, 1, 2, 1, 4, 6, 6, 3,};
+  System.out.println(findElementThatAppearOnce(arr));
+ }
+ 
+ private static int findElementThatAppearOnce(int arr[]){
+ 
+  int firstTimeOccurence = 0;
+  int secondTimeOccurence = 0;
+ 
+  for (int i=0; i < arr.length; i++){
+   secondTimeOccurence =  secondTimeOccurence | (firstTimeOccurence & arr[i]);
+   firstTimeOccurence = firstTimeOccurence ^ arr[i]; 
+   int neutraliser = ~(firstTimeOccurence & secondTimeOccurence);
+   firstTimeOccurence = firstTimeOccurence & neutraliser;
+   secondTimeOccurence = secondTimeOccurence & neutraliser;
+  }
+  return firstTimeOccurence;
+ }
+}
+
