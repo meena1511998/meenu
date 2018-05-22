b318033fb311646036810366c7d1eaf993ad1d5b
meena1511998 <37071102+meena1511998@users.noreply.github.com>
2018-05-22 10:48:19 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+class ReverseString
+{
+	public static void main(String[] args)
+	{
+		String input = "dharu";
+		char[] try1 = input.toCharArray();
+
+		for (int i = try1.length-1; i>=0; i--)
+			System.out.print(try1[i]);
+	}
+}
