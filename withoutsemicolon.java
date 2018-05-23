8c214e388fd368935954a12b9aaa8a57867bd826
meena1511998 <37071102+meena1511998@users.noreply.github.com>
2018-05-23 14:44:09 +0530

    Create withoutsemicolon.java

---
+++ withoutsemicolon.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class PrintWithoutSemiColon 
+{
+
+    public static void main(String[] args) 
+    {
+        if (System.out.printf("Hello World!\n") == null) 
+        {}
+
+        if (System.out.append("Hello World!\n").equals(null))
+        {}
+    }
+}
