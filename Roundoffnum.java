23f7043e0e794c00350ba882da9312ee8ea26efe
meena1511998 <37071102+meena1511998@users.noreply.github.com>
2018-05-23 16:25:34 +0530

    Create Roundoffnum.java

---
+++ Roundoffnum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyRoundnum 
+{
+ 
+    public static void main(String a[])
+    {
+        System.out.println("23.2 after rounding: "+Math.round(15.2));
+        System.out.println("16.8 after rounding: "+Math.round(32.8));
+        System.out.println("15.5 after rounding: "+Math.round(10.6));
+    }
+}
