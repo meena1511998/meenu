6e4372c46ddfefe81ee0a9525f3d71786e6b0314
meena1511998 <37071102+meena1511998@users.noreply.github.com>
2018-05-23 10:37:05 +0530

    Create comparison.java

---
+++ comparison.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Teststringcomparison
+{  
+ public static void main(String args[])
+ {  
+   String s1="baby";  
+   String s2="baby";  
+   String s3=new String("baby");  
+   String s4="balu";  
+   System.out.println(s1.equals(s2));
+   System.out.println(s1.equals(s3)); 
+   System.out.println(s1.equals(s4));
+ }  
+}  
