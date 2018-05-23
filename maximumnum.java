f0ee508204a0f86f8b63174657b96752f0c45279
meena1511998 <37071102+meena1511998@users.noreply.github.com>
2018-05-23 14:10:28 +0530

    Update maximumnum.java

--- maximumnum.java
+++ maximumnum.java
@@ -1,26 +1,27 @@
+
 import java.util.Scanner;
-public class maximum 
+public class highestnumber
 {
-    public static void main(String[] args) 
-    {
-        int n, max;
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter number of elements in the array:");
-        n = s.nextInt();
-        int a[] = new int[n];
-        System.out.println("Enter elements of array:");
-        for(int i = 0; i < n; i++)
-        {
-            a[i] = s.nextInt();
-        }
-        max = a[0];
-        for(int i = 0; i < n; i++)
-        {
-            if(max < a[i])
-            {
-                max = a[i];
-            }
-        }
-        System.out.println("Maximum value:"+max);
+    public static void main (String[] args) {
+  Scanner input = new Scanner(System.in);
+ int total=0;
+  int numbers;
+  int hight;
+        int counter=0;
+ {
+
+    while (counter < 10){
+
+            numbers = input.nextInt();
+
+            total = total + numbers;
+
+            counter++;
     }
+
+        high = total;
+   System.out.println("The hightest number entered is "+ high);
+    }
+ }
+
 }
