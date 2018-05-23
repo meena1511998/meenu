36dce0324945353518d448b1db6afa37088cc9da
meena1511998 <37071102+meena1511998@users.noreply.github.com>
2018-05-23 16:34:22 +0530

    Create stringlength.java

---
+++ stringlength.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Length
+{  
+public static void main(String args[])
+{  
+String s1="howareyou";  
+String s2="morning";  
+System.out.println("string length is: "+s1.length());
+System.out.println("string length is: "+s2.length());
+}}  
