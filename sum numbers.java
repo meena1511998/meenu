a5e3c3ae5fec4d7de1c86fca113ca1960ff803f0
meena1511998 <37071102+meena1511998@users.noreply.github.com>
2018-05-23 11:34:33 +0530

    Create sum numbers.java

---
+++ sum numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class AddTwoNumbers 
+{
+
+   public static void main(String[] args) 
+   {
+        
+      int num1 = 10, num2 = 29, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
