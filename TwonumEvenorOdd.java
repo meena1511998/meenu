f8f20a4ce0b15fa0f93d0fa341393f9f8961fce9
meena1511998 <37071102+meena1511998@users.noreply.github.com>
2018-05-23 15:41:37 +0530

    Update TwonumEvenorOdd.java

--- TwonumEvenorOdd.java
+++ TwonumEvenorOdd.java
@@ -1,17 +1,19 @@
-import java.util.Scanner;
- 
-class OddOrEven
+import java.util.*;
+public class EvenOdd
+{  public void check(int number)
 {
-   public static void main(String args[])
-   {
-      int x;
-      System.out.println("Enter an integer to check if it is odd or even ");
-      Scanner in = new Scanner(System.in);
-      x = in.nextInt();
- 
-      if ( x % 2 == 0 )
-         System.out.println("You entered an even number.");
-      else
-         System.out.println("You entered an odd number.");
-   }
+
+       if (number % 2 == 0) {
+
+           System.out.println(number + " is an Even Number");
+
+        } else {
+
+           System.out.println(number + " is an Odd Number");
+
+        }
+
+    }
+
 }
+
