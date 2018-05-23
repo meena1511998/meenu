8db37d2087042d056ba23973a105aca44cea170f
meena1511998 <37071102+meena1511998@users.noreply.github.com>
2018-05-23 09:10:33 +0530

    Create count length.java

---
+++ count length.java
@@ -0,0 +1,13 @@
+import java.util.scanner;
+int count = 0;
+
+for (int j = 0; j < input1.length(); j++)
+{
+  char character = input1.charAt(j);
+  count++;
+}
+
+if (indexX != -1)
+  {
+     System.out.println("x count: "+count);
+  }
