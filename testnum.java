b9ec341e0b83fe78b014f0f459e996c87bb6a924
meena1511998 <37071102+meena1511998@users.noreply.github.com>
2018-05-24 12:36:54 +0530

    Create testnum.java

---
+++ testnum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class floortest
+{ 
+
+   public static void main(String args[])
+   {
+      double d = -100.675;
+      float f = -90;
+
+      System.out.println(Math.floor(d));
+      System.out.println(Math.floor(f)); 
+
+      System.out.println(Math.ceil(d));
+      System.out.println(Math.ceil(f));
+   }
+}
