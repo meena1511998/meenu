72c4fa0d77e38e3f623529635af5aed6cdf3bd53
meena1511998 <37071102+meena1511998@users.noreply.github.com>
2018-05-24 14:00:24 +0530

    Update compositenum.java

--- compositenum.java
+++ compositenum.java
@@ -1,19 +1,27 @@
-import java.util.*;
-
-class composite 
+import java.util.Scanner;
+class Composite_Number
 {
-
-static void check(int a)
-{
-int n=0;
-
-for(int i=1;i<=a;i++)
-{
-if(a%i==0)
-n++;}
-if(n>2)
-System.out.println(a+” is a composite number”);
-else
-System.out.println(a+” is a prime number”);
+   int flag,n,i,j;
+   Composite_Number()
+     {
+       System.out.print("Enter range to find the composite numbers: ");
+       Scanner in=new Scanner(System.in);
+       n=in.nextInt();
+       for(i=2;i<=n;i++ )
+        {
+            flag=0;
+           for(j=2;j<i;j++)
+             {
+                 if(i % j == 0)
+                    flag++;
+             }
+           if(flag!=0)
+            System.out.println("one of the composite number is:" + i);
+        }
+    }
+public static void main(String args[])    
+      {
+           Composite_Number ob=new Composite_Number();
+      }
 }
-}
+
